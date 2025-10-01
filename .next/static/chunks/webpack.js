4e796e1225fc1d53ff488edb9e9a07ac264616d6
salim mahroug <mahrougsalim8@gmail.com>
2025-10-01 01:24:19 +0100

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "62efa18d6977471b"; }
+/******/ 		__webpack_require__.h = function() { return "837a8b1da1ab4c5c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
